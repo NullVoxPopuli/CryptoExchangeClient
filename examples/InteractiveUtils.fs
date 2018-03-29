a8d6c87eda37e29ed622660c8f69905f655afbb9
L. Preston Sego III <LPSego3+dev@gmail.com>
2018-03-29 14:12:33 -0700

    now with colors

--- examples/InteractiveUtils.fs
+++ examples/InteractiveUtils.fs
@@ -8,28 +8,28 @@
 
 
 open System
+open System.Drawing
+open Colorful
 
 
 module InteractiveUtils =
+    let titleColor = Color.BlueViolet
+    let optionsColor = Color.DarkSlateGray
 
     let WriteHeader (text: string) =
-        Console.ForegroundColor = ConsoleColor.DarkMagenta 
-        |> ignore
-
-        Console.WriteLine "============================="
-        Console.WriteLine text
-        Console.WriteLine "============================="
+        Console.WriteLine ("=============================", titleColor)
+        Console.WriteLine (text, titleColor)
+        Console.WriteLine ("=============================", titleColor)
         Console.WriteLine ""
-        Console.ResetColor
 
-    let PromptToContinue =
-        Console.Write("Continue? (Y|N)");
-        let key = Console.ReadLine();
+    //let PromptToContinue =
+    //    Console.Write("Continue? (Y|N)");
+    //    let key = Console.ReadLine();
         
-        match key with
-        | "n" -> Environment.Exit(0)
-        | "N" -> Environment.Exit(0)
-        | _ -> ()
+    //    match key with
+    //    | "n" -> Environment.Exit(0)
+    //    | "N" -> Environment.Exit(0)
+    //    | _ -> ()
 
     let NumberEqualsMapEntry (num: string)  (entry: string * string * (unit -> unit)): bool =
         let (number, _title, _fun) = entry
@@ -38,7 +38,8 @@ module InteractiveUtils =
 
     let PrintEntry (entry: string * string * (unit -> unit)): unit =
         let (num, title, _fun) = entry
-        Console.WriteLine(num + " )    " + title)
+
+        Console.WriteLine(num + " )    " + title, optionsColor)
 
     let FindSelected (selected: string, optionMap: (string * string * (unit -> unit)) list) =
         let finder = NumberEqualsMapEntry(selected)
@@ -49,15 +50,16 @@ module InteractiveUtils =
     let rec PromptMenu (title: string, optionMap: (string * string * (unit -> unit)) list) =
         Console.WriteLine ""
         WriteHeader title
-        Console.WriteLine "Choose a number to perform the example"
-        Console.WriteLine "NOTE: any request requiring auth will require the api key be passed to the RestClient"
+        Console.WriteLine ("Choose a number to perform the example", Color.Gray)
+        Console.WriteLine ("NOTE: any request requiring auth will require the api key be passed to the RestClient", Color.Gray)
         Console.WriteLine ""
-        Console.WriteLine "type 'exit' to exit"
+        Console.WriteLine ("type 'exit' to exit", Color.Yellow)
         Console.WriteLine ""
 
 
         optionMap |> List.iter PrintEntry
-      
+        Console.WriteLine ""
+
         let key = Console.ReadLine()        
 
         match key with
